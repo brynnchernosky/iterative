3b254b4e4210f175e5a35fb16dfac2e86a9d90bf
brynnchernosky <56202540+brynnchernosky@users.noreply.github.com>
2022-11-05 21:38:58 -0400

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <div id="appHeaderContainer">
+        <h1>Iterative Design</h1>
+      </div>
+      <div id="menuContainer">{/* TODO: Add menu */}</div>
+      <hr className="lineBreakBetweenSections" />
+      <div className="sectionContainer">
+        <div className="section">
+          <div id="Overview">
+            <h2>Overview</h2>
+            <p>
+              Avocademy is a 2020 startup dedicated to teaching people UI/UX
+              design. They offer an 8 week personalized UI/UX curriculum,
+              mentoring, and portfolio-building support.
+            </p>
+            <br />
+            <p>
+              Our team decided to prototype what that experience could look like
+              for users, sketching out a variety of possible experiences before
+              honing in on a final design to produce an interactive Figma
+              prototype for. We received feedback during a critique session and
+              through user testing, which allowed us to further refine our
+              prototype.
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className="lineBreakBetweenSections" />
+      <div className="sectionContainer">
+        <div className="section">
+          <div id="Initial Sketches">
+            <h2>Initial Sketches</h2>
+          </div>
+        </div>
+      </div>
+      <hr className="lineBreakBetweenSections" />
+      <div className="sectionContainer">
+        <div className="section">
+          <div id="Initial Prototype">
+            <h2>Initial Prototype</h2>
+          </div>
+        </div>
+      </div>
+      <hr className="lineBreakBetweenSections" />
+      <div className="sectionContainer">
+        <div className="section">
+          <div id="Final Prototype">
+            <h2>Final Prototype</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
